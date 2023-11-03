c343cb12c8036f84820b9d21b51c8222f704e65e
Kuba <132459354+KubaPro010@users.noreply.github.com>
2023-11-03 10:23:48 +0000

    more rds volume

--- src/pifmrds/fm_mpx.c
+++ src/pifmrds/fm_mpx.c
@@ -193,9 +193,9 @@ int fm_mpx_open(char *filename, size_t len, int raw, double preemphasis, int raw
 
 // samples provided by this function are in 0..10: they need to be divided by
 // 10 after.
-int fm_mpx_get_samples(float *mpx_buffer, int drds, float compressor_decay, float compressor_attack, float compressor_max_gain_recip, int disablestereo, float gain, int enablecompressor, int rds_ct_enabled) {
+int fm_mpx_get_samples(float *mpx_buffer, int drds, float compressor_decay, float compressor_attack, float compressor_max_gain_recip, int disablestereo, float gain, int enablecompressor, int rds_ct_enabled, float rds_volume) {
     int stereo_capable = (channels > 1) && (!disablestereo); //chatgpt
-    if(!drds) get_rds_samples(mpx_buffer, length, stereo_capable, rds_ct_enabled, 1.0);
+    if(!drds) get_rds_samples(mpx_buffer, length, stereo_capable, rds_ct_enabled, rds_volume);
 
     if(inf == NULL) return 0; // if there is no audio, stop here
     
